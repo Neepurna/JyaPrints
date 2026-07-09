d352e8b667f93859e232a7b81b84d66da66b71ef
Neepurna <neepurna@gmail.com>
2026-07-09 19:44:36 +0545

    Replace utility seal with JYA Store logo

--- components/TopUtilityBar.tsx
+++ components/TopUtilityBar.tsx
@@ -1,11 +1,14 @@
 import { Recycle } from "lucide-react";
+import Image from "next/image";
 import { Badge } from "@/components/ui/Badge";
 import { CircularMark } from "@/components/ui/CircularMark";
 
 export function TopUtilityBar() {
   return (
     <section className="utility-bar">
-      <CircularMark />
+      <div className="utility-brand-mark">
+        <Image src="/images/brand/jya-store-logo.png" alt="JYA Store logo" fill sizes="120px" priority />
+      </div>
       <div className="utility-copy utility-copy--left"><Recycle /><span>STREETWEAR DESIGN<br />VOL. 01 // SEASON 1</span></div>
       <div className="utility-copy utility-copy--center"><b>MYTHOLOGY DESIGN</b><span>ROOTED IN KATHMANDU</span></div>
       <div className="utility-badges"><Badge>2026</Badge><Badge>CULTURAL<br />SACRED CONTENT</Badge></div>
